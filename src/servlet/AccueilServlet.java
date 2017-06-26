8a38b49311f1906357b5ee00b4857912daa3ca9f
chatellierclement <chatellierclement44@gmail.com>
2017-06-26 16:26:57 +0200

    Page accueil

---
+++ src/servlet/AccueilServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet ( "/accueil" )
+public class AccueilServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		generate(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		generate(req, resp);
+	}
+	
+	private void generate(HttpServletRequest req, HttpServletResponse resp) {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/index.jsp");
+		try {
+			dispatcher.forward(req, resp);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
