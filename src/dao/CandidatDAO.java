5e9494de19d759f0adc7070bfa4c79cb414fe295
chatellierclement <chatellierclement44@gmail.com>
2017-06-27 10:26:03 +0200

    commit bo dao

--- src/dao/CandidatDAO.java
+++ src/dao/CandidatDAO.java
@@ -1,24 +1,35 @@
-/*package dao;
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import conf.ConnectionManager;
 import bo.CandidatBO;
 
 public class CandidatDAO {
 
-	public CandidatBO selectAll() {
-		//Connection con = models.Connect.Connection();
-		//Statement state = con.createStatement();
-		ResultSet res = state
-				.executeQuery("SELECT * FROM visiteur WHERE VIS_MATRICULE='"
-						+ log + "' AND VIS_NOM ='" + mdp + "'");
+	public CandidatBO selectOneByNomPrenom() throws SQLException {
+		CandidatBO candidatBO = null;
+		Connection con = ConnectionManager.getConnection();
+		Statement state = con.createStatement();
+		ResultSet res = state.executeQuery("SELECT * FROM visiteur WHERE VIS_MATRICULE='"
+						+ "' AND VIS_NOM ='" + "'");
 
 		if (res.next()) {
-			String login = res.getString("VIS_MATRICULE");
-			String pass = res.getString("VIS_NOM");
-			CandidatBO candidatBO = new CandidatBO(login, pass);
+			String login = res.getString("");
+			String pass = res.getString("");
+			candidatBO = new CandidatBO();
+			candidatBO.setId(1);
+			candidatBO.setNom("");
+			candidatBO.setPrenom("");
 		}
 
 		res.close();
 		state.close();
 		con.close();
+		
+		return candidatBO;
 	}
-}*/
+}
