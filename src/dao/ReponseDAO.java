0486f83c161e76cd9d4cdc957d7ffc0f1a0d6429
jlegendre <jeremy.legendre@free.fr>
2017-06-28 16:47:24 +0200

    reponseDAO

--- src/dao/ReponseDAO.java
+++ src/dao/ReponseDAO.java
@@ -2,24 +2,49 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import bo.CategorieBO;
+import bo.QuestionBO;
 import bo.ReponseBO;
 import conf.ConnectionManager;
 
 public class ReponseDAO {
 
-	public static void enregistrer(ReponseBO reponse) throws SQLException {
+	public static List<ReponseBO> selectAllByQuestion(int id)
+			throws SQLException {
+		ReponseBO pReponse = null;
+		QuestionBO pQuestion = null;
+		List<ReponseBO> pListeReponse = new ArrayList<ReponseBO>();
+
 		Connection con = ConnectionManager.getConnection();
-		String req = "INSERT INTO QUESTION VALUES (?, ?) ";
+		String req = "select * from Reponse where idQuestion = ? ";
 		PreparedStatement stmt;
-		
 		stmt = con.prepareStatement(req);
-		stmt.setString(1, reponse.getLibelle());
-		stmt.setInt(3, reponse.getQuestion().getId());
-		stmt.executeUpdate();
-		
+		stmt.setInt(1, id);
+		ResultSet res = stmt.executeQuery();
+
+		while (res.next()) {
+			pReponse = new ReponseBO();
+			pQuestion = new QuestionBO();
+
+			pQuestion.setId(id);
+
+			pReponse.setId(res.getInt(1));
+			pReponse.setLibelle(res.getString(2));
+			pReponse.setEtat(res.getInt(3));
+			pReponse.setIdQuestion(pQuestion);
+
+			pListeReponse.add(pReponse);
+		}
+
+		res.close();
 		stmt.close();
 		con.close();
+
+		return pListeReponse;
 	}
 }
