c2293f5ae25837a0860dff9d5fbe14491f6ba975
Radhan <radhan@users.noreply.github.com>
2017-06-28 14:30:40 +0200

    Test

--- src/bo/TestBO.java
+++ src/bo/TestBO.java
@@ -2,31 +2,36 @@ package bo;
 
 public class TestBO {
 	
+	private int id;
+	private String libelle;
+	private CategorieBO categorie;
+	
 	public TestBO() {
 		
 	}
-	
-	private int id;
-	private String libelle;
-	private CategorieBO idCategorie;
-	
+
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getLibelle() {
 		return libelle;
 	}
+	
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
 	}
-	public CategorieBO getIdCategorie() {
-		return idCategorie;
+	
+	public CategorieBO getCategorie() {
+		return categorie;
 	}
-	public void setIdCategorie(CategorieBO idCategorie) {
-		this.idCategorie = idCategorie;
+	
+	public void setCategorie(CategorieBO categorie) {
+		this.categorie = categorie;
 	}
 	
 	
