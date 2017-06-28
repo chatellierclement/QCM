8c308935791bc0786f8db691045542762aa08d89
jlegendre <jeremy.legendre@free.fr>
2017-06-28 10:33:09 +0200

    maj testbo

--- src/bo/TestBO.java
+++ src/bo/TestBO.java
@@ -8,6 +8,8 @@ public class TestBO {
 	
 	private int id;
 	private String libelle;
+	private CategorieBO idCategorie;
+	
 	public int getId() {
 		return id;
 	}
@@ -19,6 +21,12 @@ public class TestBO {
 	}
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
+	}
+	public CategorieBO getIdCategorie() {
+		return idCategorie;
+	}
+	public void setIdCategorie(CategorieBO idCategorie) {
+		this.idCategorie = idCategorie;
 	}
 	
 	
