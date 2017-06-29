51dfaeb34391ce5bd3895e9f5528d12136435db7
jlegendre <jeremy.legendre@free.fr>
2017-06-29 11:13:43 +0200

    dao

--- src/bo/ReponseUtiBO.java
+++ src/bo/ReponseUtiBO.java
@@ -7,4 +7,24 @@ public class ReponseUtiBO {
 	private QuestionBO question;
 	
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	public QuestionBO getQuestion() {
+		return question;
+	}
+	public void setQuestion(QuestionBO question) {
+		this.question = question;
+	}
+	
+	
 }
